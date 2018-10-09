d6ca29e62d37459f5581c4917dd0531f329ef0f0
Dave Gallant <davegallant@gmail.com>
2018-10-08 22:12:29 -0400

    change the way version is imported.

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 import io  # for python2
 from os import path
 from setuptools import setup, find_packages
-
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-from version import __version__ as version
+from ickafka.__version__ import version
 
 WORKING_DIR = path.abspath(path.dirname(__file__))
 
@@ -14,18 +9,13 @@ WORKING_DIR = path.abspath(path.dirname(__file__))
 with io.open(path.join(WORKING_DIR, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-# read requirements.txt and load into list
-REQUIREMENTS_TXT = parse_requirements(
-    path.join(WORKING_DIR, "requirements.txt"), session="my_session"
-)
-REQUIREMENTS = [str(r.req) for r in REQUIREMENTS_TXT]
 
 setup(
     author="Dave Gallant",
     author_email="davegallant@gmail.com",
     description="Improved Color Kafka",
     entry_points={"console_scripts": ["ickafka=ickafka.app:main"]},
-    install_requires=REQUIREMENTS,
+    install_requires=["kafka-python>=1.3.1", "pygments>=2.2.0"],
     keywords=["kafka", "pygments"],
     license="Apache License, Version 2.0",
     long_description=long_description,
